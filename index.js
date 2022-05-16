d024be079008de1354bb4b3507a9b006d46e50f9
Oryam <48219965+or-yam@users.noreply.github.com>
2022-05-16 08:57:29 +0300

    Add log features

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import cliProgress from 'cli-progress';
-import chalk from 'chalk';
-// import chalkAnimation from 'chalk-animation';
+import logSaber from './logSaber.js';
+import program from './program.js';
+import logText from './logText.js';
 
-const colorsOptions = new Set(['red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'white']);
+const sleep = (ms = 500) => new Promise(r => setTimeout(r, ms));
 
-program
-  .name('CLightsaber')
-  .description('CLightsaber is a CLI tool to generate lightsabers')
-  .argument('[color]', 'Color of lightsaber', 'blue');
-
-program.parse(process.argv);
 const [selectedColor] = program.args;
 
-function logSaber(color) {
-  color = color?.toLowerCase() || 'blue';
-
-  if (!colorsOptions.has(color)) {
-    console.log(chalk.bgRedBright(`The color "${selectedColor}" is not available`));
-    process.exit(1);
-  }
-
-  const saber = new cliProgress.SingleBar({
-    format: `==|${chalk[color || 'blue']('{bar})')}`,
-    barCompleteChar: '\u2588',
-    barIncompleteChar: ' ',
-    hideCursor: true
-  });
-
-  saber.start(100, 0);
-
-  setInterval(() => {
-    saber.update(++saber.value);
-    if (saber.value >= saber.total) {
-      saber.stop();
-      process.exit(0);
-    }
-  }, 5);
-}
-
+logText('CLightsaber');
+await sleep();
 logSaber(selectedColor);
---
+++ logText.js
@@ -0,0 +1,14 @@
+import gradient from 'gradient-string';
+import figlet from 'figlet';
+
+function logText(text) {
+  figlet(text, {}, (err, data) => {
+    if (err) {
+      console.log('Something went wrong...');
+      return;
+    }
+    console.log(gradient.fruit.multiline(data));
+  });
+}
+
+export default logText;
