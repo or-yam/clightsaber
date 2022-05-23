911ad088c2c9f8375a2c210ce8e75c35d7f379ac
Oryam <48219965+or-yam@users.noreply.github.com>
2022-05-23 14:51:39 +0300

    Add cli help text and errors

--- src/program.ts
+++ src/program.ts
@@ -1,9 +1,25 @@
 import { program } from 'commander';
+import chalk from 'chalk';
+import { colorsOptions } from './colors.js';
 
 program
   .name('CLightsaber')
   .description('CLightsaber is a CLI tool to generate lightsabers')
   .argument('[color]', 'Color of lightsaber', 'blue');
+
+program.addHelpText(
+  'after',
+  `
+  Optional colors:
+  ${colorsOptions.map(color => `${chalk[color](color)}`).join(' ')}
+
+  Example of usage:
+   $ cliightsaber red
+  `
+);
+
+program.showHelpAfterError('There was a disturbance in the force.');
+program.showSuggestionAfterError();
 
 program.parse(process.argv);
 
